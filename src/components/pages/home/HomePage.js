f64131783092ddfb07d5eac6a67e8bb03ed6bb0b
lletierce <letierce.lo@gmail.com>
2023-03-22 15:23:42 +0100

    💄 F01 : HOME > Hompage > Add grid layouts

--- src/components/pages/home/HomePage.js
+++ src/components/pages/home/HomePage.js
@@ -3,13 +3,28 @@ import styled from 'styled-components';
 export default function HomePage() {
   return (
     <HomePageStyled>
-      <div>Header</div>
-      <div>MainContent</div>
-      <div>Footer</div>
+      <div className='header'>Header</div>
+      <div className='mainContent'>MainContent</div>
+      <div className='footer'>Footer</div>
     </HomePageStyled>
   )
 }
 
 const HomePageStyled = styled.div`
-  background: red;
+  height: 100vh;
+  display: grid;
+  grid-template-areas:   "header"
+                        "maincontent"
+                        "footer";
+  grid-template-rows:    84px 1fr 122px;
+
+  .header{
+    border: 1px solid purple;
+  }
+  .mainContent{
+    border: 1px solid purple;
+  }
+  .footer{
+    border: 1px solid purple;
+  }
 `;
