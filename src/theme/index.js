bf8f903dcf749fa9cff65a71bfb3798ba873df67
lletierce <letierce.lo@gmail.com>
2023-03-23 15:29:18 +0100

    💄 Add theme > index.js

---
+++ src/theme/index.js
@@ -0,0 +1,53 @@
+const colors = {
+    white: "##FFFFFF",
+    whiteSecondary: "rgba(255, 255, 255, 0.7)",
+    background_white: "#f5f5f7",
+    background_dark: "#121212",
+    primary: "#17161a",
+    green: "green",
+    success: "#60bd4f",
+    red: "#e25549",
+    redSecondary: "#c4151c",
+    blue: "#51a7e1",
+  }
+  
+  const spacing = {
+    xxs: "4px",
+    xs: "8px",
+    sm: "12px",
+    md: "20px",
+    lg: "32px",
+    xl: "52px",
+    xxl: "84px",
+  }
+  
+  const fonts = {
+    size: {
+      XXXS: "8px",
+      XXS: "10px",
+      XS: "12px",
+      SM: "15px",
+      P0: "16px",
+      P1: "18px",
+      P2: "20px",
+      P3: "24px",
+      P4: "36px",
+      P5: "48px",
+      P6: "60px",
+    },
+    weights: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      heavy: 800,
+    },
+  }
+  
+  export const theme = {
+    colors,
+    spacing,
+    fonts,
+  }
+  
