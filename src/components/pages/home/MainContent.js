8186865ead404204f7a3763bc0ffa02455918d80
lletierce <letierce.lo@gmail.com>
2023-04-01 17:25:39 +0200

    💄F01b: HOME > MainContent > Add responsive layouts

--- src/components/pages/home/MainContent.js
+++ src/components/pages/home/MainContent.js
@@ -1,11 +1,30 @@
 import styled from 'styled-components';
 
 export default function MainContent() {
-  return (
-    <MainContentStyled>MainContent</MainContentStyled>
-  )
+    return (
+        <MainContentStyled>
+            <div className="banner">Banner</div>
+            <div className="featured-collection">Collection</div>
+        </MainContentStyled>
+    )
 }
 
 const MainContentStyled = styled.div`
-  border: 1px solid red;
+  border: 1px solid blue;
+  display: grid;
+  grid-template-areas: "banner"
+                        "collection";
+  grid-template-rows: 720px 1fr;
+
+  @media screen and (max-width: 749px) {
+    grid-template-rows: 390px 1fr;
+  }
+
+  .banner{
+    border: 2px solid red;
+  }
+
+  .featured-collection{
+    border: 1px solid green;
+  }
 `;
