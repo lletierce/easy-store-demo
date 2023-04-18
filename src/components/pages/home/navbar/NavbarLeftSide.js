21a82d15204f7564e9da6d1212e86a2d975e88cb
lletierce <letierce.lo@gmail.com>
2023-04-18 15:22:29 +0200

    🚧 F01: Create <NavbarLeftSide />

---
+++ src/components/pages/home/navbar/NavbarLeftSide.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+export default function NavbarLeftSide() {
+  return (
+    <NavbarLeftSideStyled>NavbarLeftSide</NavbarLeftSideStyled>
+  )
+}
+
+const NavbarLeftSideStyled = styled.div`
+  background: red;
+`;
