5d4a6654f61a26516ef94211e786e291b1497b6c
lletierce <letierce.lo@gmail.com>
2023-05-02 17:50:21 +0200

    💄  F02a: LEGISLATION >TermsAndConditionsPage >Add responsive layout

--- src/components/pages/legislation/TermsAndConditionsPage.js
+++ src/components/pages/legislation/TermsAndConditionsPage.js
@@ -1,5 +1,55 @@
+import styled from 'styled-components'
+import Header from '../home/Header'
+import Footer from '../home/Footer'
+
 export default function TermsAndConditionsPage() {
   return (
-    <div>TermsAndConditionsPage</div>
+    <TermsAndConditionsPageStyled>
+      <Header/>
+      <div className='content'>
+        <p>
+          <strong>OVERVIEW</strong>
+          <br/>
+          This website is operated by ViRUS Project SARL. Throughout the site, the terms “we”, “us” and “our” refer to ViRUS Project SARL. ViRUS Project SARL offers this website, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.
+        </p>
+      </div>
+      <Footer/>
+    </TermsAndConditionsPageStyled>
   )
 }
+
+const TermsAndConditionsPageStyled = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-areas:  "header"
+                        "maincontent"
+                        "footer";
+  grid-template-rows:    64px 1fr 132px;
+
+  .content{
+    /* background: pink; */
+    
+    max-width: 1200px;
+    height: max-content;
+    
+    margin: 0 auto;
+    padding: 21px 15px;
+  }
+
+  @media screen and (min-width: 750px) {
+    grid-template-rows: 84px 1fr 132px;
+
+    .content{
+      padding: 28px 90px;
+    }
+  }
+
+  @media screen and (min-width: 990px) {
+    grid-template-rows: 84px 1fr 132px;
+
+    .content{
+      max-width: 726px;
+      padding: 28px 0;
+    }
+  }
+`;
