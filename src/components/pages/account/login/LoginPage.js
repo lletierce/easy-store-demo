64a8cf9b700432d13cd2f0833082e2ddb402d1af
lletierce <letierce.lo@gmail.com>
2023-05-27 15:48:48 +0200

    🚧 F02: ACCOUNT > Login > Add responsive layout for LoginPage

--- src/components/pages/account/login/LoginPage.js
+++ src/components/pages/account/login/LoginPage.js
@@ -1,5 +1,30 @@
+import styled from 'styled-components'
+import Header from '../../home/Header';
+import Footer from '../../home/Footer';
+
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <LoginPageStyled>
+      <Header/>
+      <div className="content">
+        <h1>Login</h1>
+        <div>LoginForm</div>
+      </div>
+      <Footer/>
+    </LoginPageStyled>
   )
 }
+
+const LoginPageStyled = styled.div`
+   height: 100vh;
+    display: grid;
+    grid-template-areas:"header"
+                        "maincontent"
+                        "footer";
+    grid-template-rows:    64px 1fr 132px;
+
+    
+    @media screen and (min-width: 990px) {
+        grid-template-rows: 84px 1fr 132px;
+    }
+`;
