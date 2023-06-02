9690f999f52dcfad2b82f93c483308df652772d7
lletierce <letierce.lo@gmail.com>
2023-06-02 17:34:49 +0200

    💄  F02: ACCOUNT > Login > Add responsive layout for LoginPage content div

--- src/components/pages/account/login/LoginPage.js
+++ src/components/pages/account/login/LoginPage.js
@@ -1,14 +1,26 @@
 import styled from 'styled-components'
-import Header from '../../home/Header';
-import Footer from '../../home/Footer';
+import Header from '../../home/Header'
+import Footer from '../../home/Footer'
+import { theme } from '../../../../theme'
+import TextInput from '../../../reusable-ui/TextInput'
+import PrimaryButton from '../../../reusable-ui/PrimaryButton'
+import Title from '../../../reusable-ui/Title'
 
 export default function LoginPage() {
   return (
     <LoginPageStyled>
       <Header/>
       <div className="content">
-        <h1>Login</h1>
-        <div>LoginForm</div>
+        <Title value={"Login"} className={"login-title"}/>
+        <div className="login-form">
+          login-form
+          {/* <form>
+            <TextInput label={'Email'} />
+            <TextInput label={'Password'} type={"Password"} className={"last-field"}/>
+            <div className="recover-link">Forget your password?</div>
+            <PrimaryButton label={"Sign in"} className={"btn_signin"} />
+          </form> */}
+        </div>
       </div>
       <Footer/>
     </LoginPageStyled>
@@ -16,15 +28,61 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-   height: 100vh;
-    display: grid;
-    grid-template-areas:"header"
+  height: 100vh;
+  display: grid;
+  grid-template-areas:"header"
                         "maincontent"
                         "footer";
-    grid-template-rows:    64px 1fr 132px;
+  grid-template-rows:    64px 1fr 132px;
 
+  .content{
+    background: gray;
     
-    @media screen and (min-width: 990px) {
-        grid-template-rows: 84px 1fr 132px;
+    max-width: 334px;
+    box-sizing: border-box;
+    width: 100%;
+    height: max-content;
+
+    margin: 60px auto 90px;
+    padding: 0 15px;
+  }
+
+  .content .login-form{
+    background: orange;
+  }
+
+  .content .login-title{
+    display: flex;
+    justify-content: center;
+
+    margin: 0 auto 20.1px;
+
+    text-transform: capitalize;
+    letter-spacing: 0.6px;
+  }
+
+  .content .login-form .last-field{
+    margin-bottom: 10px;
+  }
+
+  .content .login-form .btn_signin{
+    /* margin-top: calc(${theme.spacing.sm}*4); */
+    margin: calc(${theme.spacing.sm}*4) auto 15px;
+
+  }
+
+  @media screen and (min-width: 750px) {
+
+    .content{
+      max-width: 478px;
     }
+
+    .content .login-title{
+      margin: 0 auto 26.8px;
+    }
+  }
+
+  @media screen and (min-width: 990px) {
+      grid-template-rows: 84px 1fr 132px;
+  }
 `;
